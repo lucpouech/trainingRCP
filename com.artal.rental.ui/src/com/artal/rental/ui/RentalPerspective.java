5dfe8e27014f5d01ce1ed08d29f8e0a6f106a713
pouech <pouech@TALLIN3>
2016-09-20 17:14:39 +0200

    RCP060

---
+++ com.artal.rental.ui/src/com/artal/rental/ui/RentalPerspective.java
@@ -0,0 +1,38 @@
+package com.artal.rental.ui;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class RentalPerspective implements IPerspectiveFactory {
+
+	/**
+	 * Creates the initial layout for a page.
+	 */
+	public void createInitialLayout(IPageLayout layout) {
+		String editorArea = layout.getEditorArea();
+		addFastViews(layout);
+		addViewShortcuts(layout);
+		addPerspectiveShortcuts(layout);
+		layout.addView("com.artal.rental.ui.views.RentalView", IPageLayout.LEFT, 0.5f, IPageLayout.ID_EDITOR_AREA);
+		layout.addView("com.artal.rental.ui.views.RentalAgencyView", IPageLayout.LEFT, 0.83f, IPageLayout.ID_EDITOR_AREA);
+	}
+
+	/**
+	 * Add fast views to the perspective.
+	 */
+	private void addFastViews(IPageLayout layout) {
+	}
+
+	/**
+	 * Add view shortcuts to the perspective.
+	 */
+	private void addViewShortcuts(IPageLayout layout) {
+	}
+
+	/**
+	 * Add perspective shortcuts to the perspective.
+	 */
+	private void addPerspectiveShortcuts(IPageLayout layout) {
+	}
+
+}
